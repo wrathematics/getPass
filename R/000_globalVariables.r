6c2e788c4f61c8bcc11f2fb9b59b60fea2e1d390
snoweye <wccsnow@gmail.com>
2016-02-21 01:41:03 -0500

    R CMD check

---
+++ R/000_globalVariables.r
@@ -0,0 +1,6 @@
+### For R CMD check
+utils::globalVariables(c(
+  "tclvalue<-", "tkdestroy", "tktoplevel", "tktitle<-", "tclVar", "tkframe",
+  "tkpack", "tklavel", "tkentry", "tkbutton", "tkwait.window", "tclvalue",
+  "tklabel"
+))
