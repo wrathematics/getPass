d01d38265cb9311b786913f1d40f17ab71e98aff
Drew Schmidt <schmidt@math.utk.edu>
2016-04-24 23:41:32 -0400

    tweak help, enforce internal style

--- R/getPass-package.r
+++ R/getPass-package.r
@@ -1,9 +1,11 @@
 #' getPass
 #'
 #' A micro-package for reading "passwords", i.e.  reading
-#' user input with masking, so that the input is not displayed as it 
-#' is typed.  Currently we have support for RStudio, the command line
-#' (every OS), and any platform where tcltk is present.
+#' user input with masking.  The package's only exported function,
+#' \code{getPass()}, behaves similarly to R's own \code{readline()}, only
+#' the user text is not printed as it is typed.  Currently we have support
+#' for RStudio, the command line (every OS), and any platform where tcltk
+#' is present.
 #' 
 #' @references Project URL: \url{https://github.com/wrathematics/getPass}
 #' @author Drew Schmidt and Wei-Chen Chen
