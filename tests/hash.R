2e53026d8b5a0dfbfbb229477696d49fca90524f
wrathematics <wrathematics@gmail.com>
2016-12-06 07:19:50 -0500

    remove old test

--- tests/hash.R
+++
@@ -1,9 +0,0 @@
-library(getPass)
-
-set.seed(1234)
-pass <- "myPassw0rd!"
-hash <- pw_hash(pass)
-
-stopifnot(pw_check(hash, pass))
-stopifnot(!pw_check(hash, "password"))
-stopifnot(!pw_check(hash, "1234"))
