652ee905eb6b03786ab7ac25651cbffb0377ea28
Drew Schmidt <wrathematics@gmail.com>
2017-02-15 18:25:17 -0500

    native routines

---
+++ src/getPass_native.c
@@ -0,0 +1,20 @@
+/* Automatically generated. Do not edit by hand. */
+
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+#include <stdlib.h>
+
+extern SEXP getPass_print_stderr(SEXP msg);
+extern SEXP getPass_readline_masked(SEXP msg, SEXP showstars_, SEXP noblank_);
+
+static const R_CallMethodDef CallEntries[] = {
+  {"getPass_print_stderr", (DL_FUNC) &getPass_print_stderr, 1},
+  {"getPass_readline_masked", (DL_FUNC) &getPass_readline_masked, 3},
+  {NULL, NULL, 0}
+};
+void R_init_getPass(DllInfo *dll)
+{
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+}
