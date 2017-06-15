567e1508aa6f8cfbfa949d540ccbfac166270da8
wrathematics <wrathematics@gmail.com>
2017-06-15 11:04:13 -0400

    fix vignette build script

--- vignettes/build_pdf.sh
+++ vignettes/build_pdf.sh
@@ -5,20 +5,24 @@ cleanVignette(){
 }
 
 buildVignette(){
+  cleanVignette
+  
   pdflatex $1
-  bibtex $1
-  #makeindex $1
-  #pdflatex $1
+  bibname=`echo "$1" | sed -e 's/\..*//'`
+  bibtex $bibname
   pdflatex $1
   pdflatex $1
   Rscript -e "tools::compactPDF('$1', gs_quality='ebook')"
 }
 
+publish(){
+  mv -f *.pdf ../inst/doc/
+  cp -f *.Rnw ../inst/doc/
+}
 
-cleanVignette
+
+
 buildVignette getPass.Rnw
+
 cleanVignette
-
-
-mv -f *.pdf ../inst/doc/
-cp -f *.Rnw ../inst/doc/
+publish
